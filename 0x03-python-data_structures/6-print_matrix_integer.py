9defbf3d2ef9f42de845de25a2b5c54818e1ff31
Reid-T-W <rediettadesse100@gmail.com>
2022-07-04 08:53:33 -0700

    Null check added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in range(len(row)):
-            if (num != len(row) - 1):
-                print("{:d}".format(row[num]), end=" ")
-            else:
-                print("{:d}".format(row[num]))
+    if matrix:
+        for row in matrix:
+            for num in range(len(row)):
+                if (num != len(row) - 1):
+                    print("{:d}".format(row[num]), end=" ")
+                else:
+                    print("{:d}".format(row[num]))
