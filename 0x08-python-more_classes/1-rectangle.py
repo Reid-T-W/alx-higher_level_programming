91feb0deaace21d7dad2f0b858431ba255ed09a4
Reid-T-W <rediettadesse100@gmail.com>
2022-07-25 22:50:36 +0300

    pycodestyle format

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,14 +4,16 @@
 
 class Rectangle:
     '''This class defines a rectangle'''
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         '''Instantiation with optional width and height'''
         self.height = height
         self.width = width
+
     @property
     def height(self):
         '''width Getter'''
         return self.__height
+
     @height.setter
     def height(self, value):
         '''width Setter'''
@@ -22,10 +24,12 @@ class Rectangle:
                 raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
+
     @property
     def width(self):
         '''height getter'''
         return self.__width
+
     @width.setter
     def width(self, value):
         '''height setter'''
@@ -36,9 +40,3 @@ class Rectangle:
                 raise ValueError("height must be >= 0")
         else:
             raise TypeError("height must be an integer")
-            
-        
-            
-   
-        
-
