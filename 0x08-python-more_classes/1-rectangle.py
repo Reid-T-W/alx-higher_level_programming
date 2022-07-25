eaf3e05271d6c131f17b2fcc139417620308ba16
Reid-T-W <rediettadesse100@gmail.com>
2022-07-25 23:11:09 +0300

    Modifications in raising exceptions

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,9 +21,9 @@ class Rectangle:
             if (value > 0):
                 self.__height = value
             else:
-                raise ValueError("width must be >= 0")
-        #else:
-         #   raise TypeError("width must be an integer")
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
 
     @property
     def width(self):
@@ -37,6 +37,6 @@ class Rectangle:
             if (value > 0):
                 self.__width = value
             else:
-                raise ValueError("height must be >= 0")
-        #else:
-           # raise TypeError("height must be an integer")
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
