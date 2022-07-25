bc7aa98e5f586306a284d5afc4cdf3645f7e38f9
Reid-T-W <rediettadesse100@gmail.com>
2022-07-26 00:09:37 +0300

    Pycodestyle format

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,12 +40,12 @@ class Rectangle:
                 raise ValueError('width must be >= 0')
         else:
             raise TypeError('width must be an integer')
- 
+
     def area(self):
         return (self.__height * self.__width)
 
     def perimeter(self):
         if (self.__height == 0 or self.__width == 0):
             return 0
-    
+
         return((2 * self.__height) + (2 * self.__width))
