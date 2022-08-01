6ec854f6e0f9778965daf5c6570ff69350fadb1f
Reid-T-W <rediettadesse100@gmail.com>
2022-08-01 22:31:58 +0300

    Module doc string added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''This module includes a function that verifies if an object is an instace of a class'''
 def is_same_class(obj, a_class):
     '''This function check if an object is an instace of a class'''
     if (isinstance(obj, a_class):
