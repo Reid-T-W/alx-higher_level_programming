5b3cbbbf037ff9bb3e2b18c0af67bb72f39ead04
Reid-T-W <rediettadesse100@gmail.com>
2022-08-01 22:30:23 +0300

    Function doc string added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
+    '''This function check if an object is an instace of a class'''
     if (isinstance(obj, a_class):
             return True
     return False
