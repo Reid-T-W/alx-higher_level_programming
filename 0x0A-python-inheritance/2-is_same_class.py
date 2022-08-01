420883e35b1d42055ffd652ee03468637b224bfb
Reid-T-W <rediettadesse100@gmail.com>
2022-08-01 22:44:42 +0300

    Correction on object type checking

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-'''This module includes a function that verifies if an object is an instace of a class'''
+'''This module includes a function that verifies if an object is an instace'''
+
+
 def is_same_class(obj, a_class):
     '''This function check if an object is an instace of a class'''
-    if (isinstance(obj, a_class):
-            return True
+    if (type(obj) is a_class):
+        return True
     return False
+
+
+if __name__ == "__main__":
+    is_same_class(obj, a_class)
