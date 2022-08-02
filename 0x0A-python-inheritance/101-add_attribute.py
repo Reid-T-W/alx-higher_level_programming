37b467b5d310d3d29aa9e77492733418962ca61e
Reid-T-W <rediettadesse100@gmail.com>
2022-08-02 05:44:28 +0300

    Task 13 Add attribute if possible

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''This module contains a function that checks for builtin classes'''
+
+
+def add_attribute(obj, attr_name, attr_value):
+    setattr(obj, attr_name, attr_value)
