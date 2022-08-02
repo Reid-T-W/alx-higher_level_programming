c83ded50a20833c3414631166bf85608b43217da
Reid-T-W <rediettadesse100@gmail.com>
2022-08-03 02:05:10 +0300

    append changed to write

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     '''This function writes json to file'''
-    with open(filename, 'a', encoding="utf-8") as wFile5:
+    with open(filename, 'w', encoding="utf-8") as wFile5:
         wFile5.write(json.dumps(my_obj))
 
 
