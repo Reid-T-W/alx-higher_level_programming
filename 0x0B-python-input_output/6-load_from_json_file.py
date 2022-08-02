3ff3607a404b2d8623c7608916737391f13cf509
Reid-T-W <rediettadesse100@gmail.com>
2022-08-03 00:45:04 +0300

    doc string added for function

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
+    '''This function deserializes a json file'''
     with open(filename, encoding="utf-8") as rFile6:
         return(json.load(rFile6))
 
