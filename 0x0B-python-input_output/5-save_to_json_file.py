56bdeb691fd03c47db86117d88bec6ce54d52893
Reid-T-W <rediettadesse100@gmail.com>
2022-08-02 23:27:32 +0300

    Task 5: Save json to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This module contains a function that writes json to file'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''This function writes json to file'''
+    json_format = json.dumps(my_obj)
+    with open(filename, 'w', encoding="utf-8") as wFile5:
+        wFile5.write(json_format)
+
+
+if __name__ == "__main__":
+    save_to_json_file(my_obj, filename)
