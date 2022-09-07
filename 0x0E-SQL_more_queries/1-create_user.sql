a27c9a90d1e244326d9783ee98280d64e012dcd4
Reid-T-W <rediettadesse100@gmail.com>
2022-09-07 22:42:04 +0300

    Create user that has all privelages on mysql server

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create a user that has all privelages and authenticates with a password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
