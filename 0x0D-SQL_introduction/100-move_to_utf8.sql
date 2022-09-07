bccce3d4422fae11bed51cdcdec07443ab58515c
Reid-T-W <rediettadesse100@gmail.com>
2022-09-07 04:49:17 -0700

    table, database, and name converted to utf8 correctly

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,13 +1,11 @@
 -- convert character encoding of database, tables and fields to UTF8
--- converting tables
--- mysql \-\-database=hbtn_0c_0 -B -N -e "SHOW TABLES" | awk '{print "SET foreign_key_checks = 0; ALTER TABLE", $1, "CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; SET foreign_key_checks = 1; "}' | mysql --database=dbname
 
 -- converting database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE hbtn_0c_0
+USE hbtn_0c_0;
 
---converting table
+-- converting table
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- converting field
