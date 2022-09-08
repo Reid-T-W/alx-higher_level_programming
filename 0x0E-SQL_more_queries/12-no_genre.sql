5a1b20d5a6f4d9e31c85ace96837f1d55a07dc83
Reid-T-W <rediettadesse100@gmail.com>
2022-09-08 14:00:05 -0700

    Outer join changed to full join and order by added

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_show_genres
-OUTER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.show_id IS NULL OR tv_shows.id IS NULL
+FULL JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id;
+WHERE tv_show_genres.show_id IS NULL OR tv_shows.id IS NULL;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
