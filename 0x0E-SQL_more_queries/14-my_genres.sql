772e43792220ccaf1acf62232fd63e3c1dc2ff33
Reid-T-W <rediettadesse100@gmail.com>
2022-09-08 15:52:08 -0700

    lists all genres to the show dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,5 +5,5 @@ INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.name = 'Dexter'
-ORDER BY tv_shows.name
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name
