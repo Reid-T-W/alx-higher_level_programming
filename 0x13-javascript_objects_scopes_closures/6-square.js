af50029d1209e68ca626f73278f3f172e2ce5d97
Reid-T-W <rediettadesse100@gmail.com>
2022-10-04 16:34:58 +0300

    print value changed from hardcoded to param value

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -13,7 +13,7 @@ class Square extends firstSquare {
       let concat = '';
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
-          concat = concat + 'C';
+          concat = concat + c;
         }
         console.log(concat);
         concat = '';
