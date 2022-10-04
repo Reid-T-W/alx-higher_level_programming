32463c94160fb1b6ac019cccc581092967aaa27e
Reid-T-W <rediettadesse100@gmail.com>
2022-10-04 17:56:04 +0300

    reverse done manually

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  reverseArr = [];
-  arrLen = list.length;
+  const reverseArr = [];
+  const arrLen = list.length;
   for (let i = 0; i < arrLen; i++) {
     reverseArr[i] = list[arrLen - 1 - i];
   }
