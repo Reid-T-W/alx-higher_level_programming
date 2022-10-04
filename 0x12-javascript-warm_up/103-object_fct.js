8a8d264d4e9c9c9b112cc9f53502b72fb6ba76fa
Reid-T-W <rediettadesse100@gmail.com>
2022-10-04 11:51:23 +0300

    Added the function to the object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,10 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr () {
+function incr () {
   myObject.value++;
-};
+}
+myObject.incr = incr;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
