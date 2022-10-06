30a4cb20449b80d5f9b5fd6706a0beccd5b81a29
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 01:22:36 +0300

    Task 6 Scehma defined in sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ORM Script
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+            Column, ForeignKey, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+
+# Creating the engine
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
