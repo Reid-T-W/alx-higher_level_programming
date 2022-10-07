d65a522705be6b63f7c05204472840f191c28f4d
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 15:21:29 +0300

    engine added, schemas created

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,10 @@ from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
 from sqlalchemy.ext.declarative import declarative_base
 
 # Creating the engine
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                       format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                       pool_pre_ping=True)
+engine.connect()
 Base = declarative_base()
 
 
@@ -16,3 +20,6 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
