79f4693896e1a654a8fdaa24bd77cb7e242db02f
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 21:45:44 +0300

    Task 14 City schema and one to many relationship

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that adds a state to the states table
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    cities = session.query(City).all()
+    for city in cities:
+        session2 = Session(bind=engine)
+        state = session2.query(State).filter(city.state_id == State.id).first()
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
