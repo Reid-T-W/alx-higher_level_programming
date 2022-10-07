4a4a787a9bf21db04b24250140bc2bcd73a03d9b
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 15:23:27 +0300

    engine.connect removed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                        format(sys.argv[1], sys.argv[2], sys.argv[3]),
                        pool_pre_ping=True)
-engine.connect()
+
 Base = declarative_base()
 
 
