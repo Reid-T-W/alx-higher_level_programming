f014d2e44b941249f76ec0ee5b15b2d0ee307de0
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 15:28:44 +0300

    docstring added to class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Creates the State class
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
