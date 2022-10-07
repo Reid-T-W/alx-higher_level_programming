4c4dd3233e28d002b4770468636b9f56b5fa139d
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 21:47:06 +0300

    City schema

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+ORM Script
+"""
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+            Column, ForeignKey, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Creates the City class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
