f5269b2d7064690387e73f410151325afe53ea67
Reid-T-W <rediettadesse100@gmail.com>
2022-10-07 15:26:21 +0300

    create engine removed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,11 +8,6 @@ from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
             Column, ForeignKey, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
-# Creating the engine
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-
 Base = declarative_base()
 
 
