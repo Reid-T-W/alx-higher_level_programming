83881f2c249789ed6d5130e68971ce682e0aa10b
Reid-T-W <rediettadesse100@gmail.com>
2022-10-20 07:13:46 -0700

    Leading whitespace removed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accecpcurl -X HEAD -s "$1"
-curl -sX OPTIONS "$1" -i | grep -i Allow: | awk '{$1=""; print}'
+curl -sX OPTIONS "$1" -i | grep -i Allow:| awk '{$1=""; sub(/^[ \t]+/, ""); print}'
