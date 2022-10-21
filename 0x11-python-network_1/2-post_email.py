1c60a9df675b2de0f62e80fa3c7b49c5f243baab
Reid-T-W <rediettadesse100@gmail.com>
2022-10-21 21:19:39 +0300

    Pycodestyle formatted

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ import sys
 
 
 url = sys.argv[1]
-values = {'email':sys.argv[2]}
+values = {'email': sys.argv[2]}
 data = urllib.parse.urlencode(values)
 data = data.encode('ascii')
 req = urllib.request.Request(url, data)
@@ -13,7 +13,4 @@ req = urllib.request.Request(url, data)
 if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         content = response.read()
-
         print("{}".format(content.decode('utf-8')))
-       
-
