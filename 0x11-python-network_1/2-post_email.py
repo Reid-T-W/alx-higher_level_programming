a7774cb85b051002207374ab0de91f9ea9b42566
Reid-T-W <rediettadesse100@gmail.com>
2022-10-21 21:22:29 +0300

    code position rearranged

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,13 +4,12 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-
 if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
+        url = sys.argv[1]
+        values = {'email': sys.argv[2]}
+        data = urllib.parse.urlencode(values)
+        data = data.encode('ascii')
+        req = urllib.request.Request(url, data)
         content = response.read()
         print("{}".format(content.decode('utf-8')))
