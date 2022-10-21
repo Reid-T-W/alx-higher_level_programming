cdeb2213465c63da3c6770076f7cf53d2a20ee18
Reid-T-W <rediettadesse100@gmail.com>
2022-10-21 17:41:01 +0300

    Send post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a post request urllib"""
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+values = {'email':sys.argv[2]}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+
+        print("{}".format(content.decode('utf-8')))
+       
+
