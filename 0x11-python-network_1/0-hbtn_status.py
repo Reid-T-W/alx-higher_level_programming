eb6c208c6957c78db525e635fe7d441c7d0794ec
Reid-T-W <rediettadesse100@gmail.com>
2022-10-21 16:17:25 +0300

    Script will not execute if imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """This module fetches a url using urllib"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)).expandtabs(4))
-    print("\t- content: {}".format(content).expandtabs(4))
-    print("\t- utf8 content: {}".
-          format(content.decode(response.headers.get_content_charset()))
-          .expandtabs(4))
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')  \
+                                as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)).expandtabs(4))
+        print("\t- content: {}".format(content).expandtabs(4))
+        print("\t- utf8 content: {}".
+              format(content.decode(response.headers.get_content_charset()))
+              .expandtabs(4))
