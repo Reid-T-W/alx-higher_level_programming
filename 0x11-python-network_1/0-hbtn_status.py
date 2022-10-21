0f9096b8e9aabee6c6abf1a8c8058f329d7c797d
Reid-T-W <rediettadesse100@gmail.com>
2022-10-21 21:01:14 +0300

    tab spacing changed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
                                 as response:
         content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)).expandtabs(4))
-        print("\t- content: {}".format(content).expandtabs(4))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".
-              format(content.decode('utf-8'))
-              .expandtabs(4))
+              format(content.decode('utf-8')))
         print(response.headers)
