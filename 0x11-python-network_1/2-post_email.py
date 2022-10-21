b377467f76dfd7870752e54c99fa6dcffbe12e97
Reid-T-W <rediettadesse100@gmail.com>
2022-10-21 21:26:17 +0300

    req creation code moved into if statement

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,12 @@ import sys
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+
     with urllib.request.urlopen(req) as response:
-        url = sys.argv[1]
-        values = {'email': sys.argv[2]}
-        data = urllib.parse.urlencode(values)
-        data = data.encode('ascii')
-        req = urllib.request.Request(url, data)
         content = response.read()
         print("{}".format(content.decode('utf-8')))
