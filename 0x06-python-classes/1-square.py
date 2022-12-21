cce6c5898ca3311b7c91549b15532ba657bbc8e9
Reid-T-W <rediettadesse100@gmail.com>
2022-12-22 00:46:11 +0300

    initialization with private instace attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class"""
+
+
+class Square():
+    """Empty square class"""
+    def __init__(self, size):
+        """called on initialization"""
+        self.__size = size
