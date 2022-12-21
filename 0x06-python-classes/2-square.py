e4ed7b14728d4637ce6e16dac5143facfeb24f37
Reid-T-W <rediettadesse100@gmail.com>
2022-12-22 00:56:14 +0300

    validation for square size added

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Empty class"""
+
+
+class Square():
+    """Empty square class"""
+    def __init__(self, size=0):
+        """called on initialization"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
